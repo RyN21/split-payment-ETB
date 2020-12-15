0d5425a5afbfa844ea921c13482efdbd1a5f3217
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-14 22:12:55 -0700

    Create test should send split payment

--- test/splitPayment.js
+++ test/splitPayment.js
@@ -5,4 +5,21 @@ contract('SplitPayment', (accounts) => {
   before(async () => {
     splitPayment = await SplitPayment.deployed();
   })
-})
+  it('Should send split payment', async () => {
+    const recipients = [accounts[1], accounts[2], accounts[3]];
+    const amount = [40, 20, 30];
+    const initialBalances = await Promise.all(recipients.map(recipient => {
+      return web3.eth.getBalance(recipient);
+    }));
+    await splitPayment.send(recipients, amount, {from: accounts[0], value: 90});
+    const finalBalances = await Promise.all(recipients.map(recipient => {
+      return web3.eth.getBalance(recipient);
+    }));
+    recipients.forEach((_item, i) => {
+      const finalBalance = web3.eth.utils.toBN(finalBalances[i]);
+    recipients.forEach((_item, i) => {
+      const initialBalance = web3.eth.utils.toBN(initialBalances[i]);
+    assert(finalBalance.sub(initialBalance).toNumber() === amounts[i]);
+    });
+  });
+});
