96849075b709c313bfe2fb3d750afb871e27abd5
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-14 21:32:01 -0700

    Use require method to assure it is the owner making the transaction

--- contracts/SplitPayment.sol
+++ contracts/SplitPayment.sol
@@ -6,11 +6,12 @@ contract SplitPayment {
   constructor(address _owner) public {
     owner = _owner;
   }
-  
+
   function send(address payable[] memory to, uint[] memory amount)
     payable
     public
     {
+      require(msg.sender == owner, 'only owner can send transaction')
       require(to.length == amount.length, 'to and amount array must have same length.');
       for(uint i = 0; i < to.length; i++) {
         to[i].transfer(amount[i]);
